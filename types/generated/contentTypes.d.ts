e42bbda46c03c8c9f7127f603e38ec84644e1f7c
Sippakorn Kongbok <6710110440@psu.ac.th>
2025-02-27 03:10:30 +0700

    Added Teacher Collection-type And Tested Some Data

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -613,6 +613,34 @@ export interface ApiSoldSold extends Struct.CollectionTypeSchema {
   };
 }
 
+export interface ApiTeacherTeacher extends Struct.CollectionTypeSchema {
+  collectionName: 'teachers';
+  info: {
+    displayName: 'Teacher';
+    pluralName: 'teachers';
+    singularName: 'teacher';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    count: Schema.Attribute.Integer;
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::teacher.teacher'
+    > &
+      Schema.Attribute.Private;
+    publishedAt: Schema.Attribute.DateTime;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface PluginContentReleasesRelease
   extends Struct.CollectionTypeSchema {
   collectionName: 'strapi_releases';
@@ -1128,6 +1156,7 @@ declare module '@strapi/strapi' {
       'api::favorite.favorite': ApiFavoriteFavorite;
       'api::payment-qr.payment-qr': ApiPaymentQrPaymentQr;
       'api::sold.sold': ApiSoldSold;
+      'api::teacher.teacher': ApiTeacherTeacher;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
       'plugin::i18n.locale': PluginI18NLocale;
